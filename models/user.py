8a2d2e1c864a3ff8dfd6d733c258f35b0b7c3a18
NsengaNigel <nsenganigel@gmail.com>
2024-09-25 02:33:13 -0700

    user v2

--- models/user.py
+++ models/user.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+"""User Module.
+
+This module defines the User class, which inherits from BaseModel and Base.
+It represents a user in the database with attributes for email, password,
+first name, and last name.
+"""
+
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """User class for users in the database."""
+
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a User instance."""
+        super().__init__(*args, **kwargs)
