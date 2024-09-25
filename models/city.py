4501fb76ad76370bc02635fed9a058e0b01d66ab
NsengaNigel <nsenganigel@gmail.com>
2024-09-25 02:05:08 -0700

    state v2

--- models/city.py
+++ models/city.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""City Module."""
+"""State Module."""
 
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
-class City(BaseModel, Base):
-    """City class for cities in the database."""
+class State(BaseModel, Base):
+    """State class for states in the database."""
 
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-
-    state = relationship("State", back_populates="cities")
+    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
 
--- models/state.py
+++ models/state.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""State Module."""
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class for states in the database."""
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+
