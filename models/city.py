541eee6bb3ad6947febf293dfabf6adcf4ab9ef6
NsengaNigel <nsenganigel@gmail.com>
2024-09-25 02:26:17 -0700

    city v2

--- models/city.py
+++ models/city.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
-
 class City(BaseModel, Base):
     """City class for cities in the database."""
 
@@ -15,4 +14,12 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
     state = relationship("State", back_populates="cities")
+
+    def __init__(self, *args, **kwargs):
+        """Initialize City instance."""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        """Return string representation of City."""
+        return f"[City] ({self.id}) {self.name}"
 
