afb13920d5cda26095f009567e733e50537a1e39
NsengaNigel <nsenganigel@gmail.com>
2024-09-25 03:12:15 -0700

    review v2

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""Review class module for AirBnB clone."""
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """Represents a review for the AirBnB clone."""
+    
+    __tablename__ = 'reviews'
+    
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+    user = relationship("User", back_populates="reviews", cascade="all, delete-orphan")
+    place = relationship("Place", back_populates="reviews", cascade="all, delete-orphan")
