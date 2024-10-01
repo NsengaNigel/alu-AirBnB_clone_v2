b7349ed30f2b8b551577cd8cf91eef2cf1b87d23
NsengaNigel <nsenganigel@gmail.com>
2024-10-01 00:37:11 -0700

    Add 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
+from fabric.api import env, put, run
+import os.path
+
+env.hosts = ['<IP web-01>', 'IP web-02']  # Replace with your server IPs
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    try:
+        # Upload the archive
+        put(archive_path, "/tmp/")
+
+        # Get the filename without extension
+        file_name = os.path.basename(archive_path)
+        folder_name = file_name.split('.')[0]
+
+        # Uncompress the archive
+        run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_name, folder_name))
+
+        # Remove the archive
+        run('rm /tmp/{}'.format(file_name))
+
+        # Move files
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'.format(folder_name, folder_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(folder_name))
+
+        # Delete the symbolic link
+        run('rm -rf /data/web_static/current')
+
+        # Create new symbolic link
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(folder_name))
+
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
