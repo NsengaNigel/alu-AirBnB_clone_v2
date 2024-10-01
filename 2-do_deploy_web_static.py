1b0c08aea1dd51bbb10ade959689d5f821f2d296
NsengaNigel <nsenganigel@gmail.com>
2024-10-01 01:03:52 -0700

    Add 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script that distributes an archive to the web servers
 """
 
-from fabric.api import env, put, run, sudo
+from fabric.api import env, put, run
 import os.path
 
-env.hosts = ['<54.146.239.100>', '<18.209.46.120>']  # Replace with your server IPs
+# Define the IP addresses of your web servers
+env.hosts = ['<54.146.239.100>', '<18.209.46.120>']
 
 
 def do_deploy(archive_path):
     """
     Distributes an archive to the web servers
     """
-    if not os.path.isfile(archive_path):
+    if not os.path.exists(archive_path):
         return False
 
     try:
@@ -22,33 +23,26 @@ def do_deploy(archive_path):
 
         # Get the filename without extension
         file_name = os.path.basename(archive_path)
-        folder_name = file_name.split('.')[0]
+        folder_name = "/data/web_static/releases/" + file_name.split('.')[0]
 
         # Uncompress the archive
-        run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
+        run('mkdir -p {}'.format(folder_name))
+        run('tar -xzf /tmp/{} -C {}/'.format(file_name, folder_name))
 
         # Remove the archive
         run('rm /tmp/{}'.format(file_name))
 
         # Move files
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
+        run('mv {}/web_static/* {}/'.format(folder_name, folder_name))
+        run('rm -rf {}/web_static'.format(folder_name))
 
-        # Delete the old symbolic link
+        # Delete the symbolic link
         run('rm -rf /data/web_static/current')
 
         # Create new symbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
-
-        # Ensure correct permissions
-        sudo('chown -R ubuntu:ubuntu /data/')
-
-        # Restart Nginx
-        sudo('service nginx restart')
+        run('ln -s {} /data/web_static/current'.format(folder_name))
 
         print("New version deployed!")
         return True
-    except Exception as e:
-        print(str(e))
+    except Exception:
         return False
