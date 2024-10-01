348028fa7d74591ba74202ffbdc778266eaf7c62
NsengaNigel <nsenganigel@gmail.com>
2024-10-01 00:26:45 -0700

    Add 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,44 +1,54 @@
 #!/usr/bin/env bash
-# Script to set up web servers for deployment of web_static
+# Sets up web servers for the deployment of web_static
 
-# Update package lists and install Nginx if not already installed
-sudo apt-get update
-sudo apt-get install -y nginx
+# Install Nginx if not already installed
+apt-get update
+apt-get -y install nginx
 
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+# Create necessary directories
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# Create a fake HTML file for testing
+# Create fake HTML file
 echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 
-# Create symbolic link, removing the old one if it exists
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate symbolic link
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the current user and group
-sudo chown -R $(whoami):$(whoami) /data/
+# Give ownership to ubuntu user and group
+chown -R ubuntu:ubuntu /data/
 
-# Ensure the Nginx configuration includes the /hbnb_static/ location block
-if ! grep -q "location /hbnb_static/" /etc/nginx/sites-available/default; then
-    sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-fi
+# Update Nginx configuration
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Test the Nginx configuration for syntax errors
-sudo nginx -t
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
+    }
 
-# Restart Nginx to apply changes if the configuration test passed
-if [ $? -eq 0 ]; then
-    sudo service nginx restart
-else
-    echo "Nginx configuration failed. Please check your configuration."
-    exit 1
-fi
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-# Exit script successfully
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
 exit 0
