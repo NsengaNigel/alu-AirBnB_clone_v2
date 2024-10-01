07e438f0655b78824ab1182a245db89d6d390634
NsengaNigel <nsenganigel@gmail.com>
2024-10-01 00:55:50 -0700

    Add changes to 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,10 +3,10 @@
 Fabric script that distributes an archive to your web servers
 """
 
-from fabric.api import env, put, run
+from fabric.api import env, put, run, sudo
 import os.path
 
-env.hosts = ['<54.146.239.100>', '18.209.46.120']  
+env.hosts = ['<54.146.239.100>', '<18.209.46.120>']  # Replace with your server IPs
 
 
 def do_deploy(archive_path):
@@ -26,26 +26,29 @@ def do_deploy(archive_path):
 
         # Uncompress the archive
         run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(file_name, folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, folder_name))
 
         # Remove the archive
         run('rm /tmp/{}'.format(file_name))
 
         # Move files
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(folder_name, folder_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(folder_name))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(folder_name, folder_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(folder_name))
 
-        # Delete the symbolic link
+        # Delete the old symbolic link
         run('rm -rf /data/web_static/current')
 
         # Create new symbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(folder_name))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+
+        # Ensure correct permissions
+        sudo('chown -R ubuntu:ubuntu /data/')
+
+        # Restart Nginx
+        sudo('service nginx restart')
 
         print("New version deployed!")
         return True
-    except Exception:
+    except Exception as e:
+        print(str(e))
         return False
